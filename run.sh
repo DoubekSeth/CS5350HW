bf2ef6058d0bbe9e8b6a46ce1fe8240c1f97a941
Seth Doubek <doubekseth@gmail.com>
2023-10-20 23:13:39 -0600

    More homework edits

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-jupyter nbconvert --execute "ToRun/run.ipynb"
+#!/bin/bash
+
+# Set the directory where your Jupyter Notebook is located
+cd ToRun
+
+# Activate your Python environment (if needed)
+# source activate your_environment
+
+# Convert the notebook to a script
+jupyter nbconvert --to script run.ipynb
+
+# Run the converted script
+python run.py
