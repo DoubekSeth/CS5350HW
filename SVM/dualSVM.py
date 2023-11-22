9c76ea6daa69037e334be6052832cf60c17cb8f3
Seth Doubek <doubekseth@gmail.com>
2023-11-22 00:22:31 -0700

    Implemented dualSVM

--- SVM/dualSVM.py
+++ SVM/dualSVM.py
@@ -15,14 +15,13 @@ def add_bias_term_to_df(df):
         df.insert(columns-1, "Bias Term", ones, False)
 
 
-def dualSVM(training, epochs, kernel, C):
+def dualSVM(training, kernel, C, gamma=1):
     #Add bias term to dataframe
     add_bias_term_to_df(training)
     #Initialize weights to 0
-    w = np.zeros(training.shape[1]-1)
     y_vec = 2*training["Label"]-1
     #For training examples, find optimal values
-    kernelMatrix = np.fromfunction(np.vectorize(lambda i, j: kernel(training.iloc[int(i)], training.iloc[int(j)])), (training.shape[0], training.shape[0]))
+    kernelMatrix = np.fromfunction(np.vectorize(lambda i, j: kernel(training.iloc[int(i), :training.shape[1]-1], training.iloc[int(j), :training.shape[1]-1], gamma)), (training.shape[0], training.shape[0]))
 
     def objective_function(X):
         return 0.5*np.sum(np.dot(kernelMatrix * np.array((y_vec * X))[:, np.newaxis], y_vec*X))-np.sum(X)
@@ -30,45 +29,67 @@ def dualSVM(training, epochs, kernel, C):
     bounds = optimize.Bounds(lb=0, ub=C)
 
     def mutualSlack(X):
-            return np.dot(X, y_vec)-0
+        return np.dot(X, y_vec)-0
         
     constraint_dict = {'type': 'eq', 'fun':mutualSlack}
 
     initial_guess = np.zeros(len(y_vec))
 
     result = optimize.minimize(objective_function, initial_guess, method="SLSQP", constraints=constraint_dict, bounds=bounds)["x"]
-    print(result)
-
+    #Set all alphas close to 0 to 0
+    epsilon = 0.00000001
+    result[result < epsilon] = 0
     return result
 
 def noKernel(a, b):
     return np.dot(a, b)
 
-def evaluate_dualSVM(testing, training, alphas):
+def gaussiankernel(a, b, c):
+    return np.e**(-(np.linalg.norm(a-b)**2)/c)
+
+def evaluate_dualSVM(testing, training, alphas, kernel, gamma=1):
     correct = 0
     add_bias_term_to_df(testing)
-    for index, row in testing.iterrows():
-        label = row.iloc[-1]
-        data = row.iloc[:len(row)-1]
-        prediction = sgn(alphas*training*np.fromfunction(lambda i, j: kernel(training)))
-        if((2*label-1)*prediction > 0):
-            correct += 1
-    return correct/testing.shape[0]
+    add_bias_term_to_df(training)
+    test_y = 2*testing["Label"]-1
+    #Create kernel matrix
+    kernelMatrix = np.fromfunction(np.vectorize(lambda i, j: kernel(training.iloc[int(i), :training.shape[1]-1], testing.iloc[int(j), :testing.shape[1]-1], gamma)), (training.shape[0], testing.shape[0]))
+    alphayis = alphas*(2*training["Label"]-1)
+    vecsgn = np.vectorize(sgn)
+    predictions = vecsgn(np.dot(alphayis, kernelMatrix))
+    errs = 0.5*np.sum(np.abs(predictions-test_y))
+    return (testing.shape[0]-errs)/testing.shape[0]
 
 def sgn(x):
     return 1 if x >= 0 else -1
 
-print(dualSVM(bank_note_training, 1, noKernel, C=10))
-# for i in range(0, 3):
-#     if(i == 0):
-#         C = 100/873
-#     if(i == 1):
-#         C = 500/873
-#     if(i == 2):
-#         C = 700/873
-#     print(i)
-#     trained_weights = primalSVM(bank_note_training, 100, C=C)
-#     print(evaluate_perceptron(bank_note_training, trained_weights))
-#     acc = evaluate_perceptron(bank_note_testing, trained_weights)
-#     print(acc)
-#     print(trained_weights)
+def recover_weights(alphas, training):
+    y_vec = 2*training["Label"]-1
+    x_vec = training[["VoW", "SoW", "CoW", "Entropy", "Bias Term"]]
+    w = np.sum(x_vec * np.array((y_vec * alphas))[:, np.newaxis], axis=0)
+    return w
+
+for i in range(0, 3):
+    if(i == 0):
+        C = 100/873
+    if(i == 1):
+        C = 500/873
+    if(i == 2):
+        C = 700/873
+    for j in range(0, 5):
+        if(j == 0):
+            gamma = 0.1
+        if(j == 1):
+            gamma = 0.5
+        if(j == 2):
+            gamma = 1
+        if(j == 3):
+            gamma = 5
+        if(j == 4):
+            gamma = 100
+        print(C, gamma)
+        alphas = dualSVM(bank_note_training, gaussiankernel, C=C, gamma=gamma)
+        print("Testing Error", evaluate_dualSVM(testing=bank_note_testing, training=bank_note_training, alphas=alphas, kernel=gaussiankernel, gamma=gamma))
+        print("Training Error", evaluate_dualSVM(testing=bank_note_training, training=bank_note_training, alphas=alphas, kernel=gaussiankernel, gamma=gamma))
+        print("Number of support vectors", np.count_nonzero(alphas))
+    #print(recover_weights(alphas, bank_note_training))
